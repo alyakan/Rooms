5ff41ba14a786e631c25c551bda3a6518e8e96a5
Aly <aly.yakan@gmail.com>
2017-07-16 16:37:26 +0200

    Removed Pods

--- Pods/Target Support Files/Pods-Rooms/Pods-Rooms-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Rooms : NSObject
-@end
-@implementation PodsDummy_Pods_Rooms
-@end
--- Pods/Target Support Files/Pods-RoomsTests/Pods-RoomsTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RoomsTests : NSObject
-@end
-@implementation PodsDummy_Pods_RoomsTests
-@end
